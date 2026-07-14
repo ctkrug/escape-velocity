9bd3beffef596963100e4df74f4a6e6e3fad9367
ctkrug <ctkrug4501@gmail.com>
2026-07-14 20:20:30 +0000

    test(particles): import fast-check ahead of property-based tests

--- tests/particles.test.js
+++ tests/particles.test.js
@@ -1,4 +1,5 @@
 import { describe, it, expect } from "vitest";
+import fc from "fast-check";
 import { createDebrisBurst, updateParticles, particleOpacity } from "../src/particles.js";
 
 function fixedRandom(sequence) {
