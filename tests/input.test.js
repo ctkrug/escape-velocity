136cf11454ec64fa841a7d03f45b7e02f99ba132
ctkrug <ctkrug4501@gmail.com>
2026-07-14 20:18:26 +0000

    test(input): import fast-check ahead of property-based tests

--- tests/input.test.js
+++ tests/input.test.js
@@ -1,4 +1,5 @@
 import { describe, it, expect } from "vitest";
+import fc from "fast-check";
 import { dragVector, dragDistance, clampToMaxDistance, launchVelocity } from "../src/input.js";
 
 describe("dragVector / dragDistance", () => {
